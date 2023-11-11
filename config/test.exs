97e4ae7f2f6dc44d39841758b7018f1e9e020bce
Adan Nogueira <adan.nogueira@gmail.com>
2023-11-11 17:00:51 -0300

    chore:🎉 initial commit

---
+++ config/test.exs
@@ -0,0 +1,27 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :banana_bank, BananaBank.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "banana_bank_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :banana_bank, BananaBankWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "ROH9n1F++YNC7kJhOv/USnJ6J9juDaXowlWbbLdzBiBF5YU3WaZIEzE1sQCeFvH/",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
