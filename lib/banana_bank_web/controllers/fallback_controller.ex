a5cdb151e60ac48ee589c2c628772793e9943cc8
Adan Nogueira <adan.nogueira@gmail.com>
2023-11-28 08:01:53 -0300

    feat:✨ add get user route

--- lib/banana_bank_web/controllers/fallback_controller.ex
+++ lib/banana_bank_web/controllers/fallback_controller.ex
@@ -1,10 +1,18 @@
 defmodule BananaBankWeb.FallbackController do
   use BananaBankWeb, :controller
+  alias BananaBankWeb.ErrorJSON
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(json: ErrorJSON)
+    |> render(:error, status: :not_found)
+  end
 
   def call(conn, {:error, changeset}) do
     conn
     |> put_status(:bad_request)
-    |> put_view(json: BananaBankWeb.ErrorJSON)
+    |> put_view(json: ErrorJSON)
     |> render(:error, changeset: changeset)
   end
 end
